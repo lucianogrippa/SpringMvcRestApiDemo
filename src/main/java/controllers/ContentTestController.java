dc478022a56fc97870e1ffbb5f005605ae8124ea
Luciano Grippa <grippa.luciano@gmail.com>
2020-01-07 16:31:47 +0100

    eliminata security alert

--- src/main/java/controllers/ContentTestController.java
+++ src/main/java/controllers/ContentTestController.java
@@ -1,6 +1,5 @@
 package controllers;
 
-import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,19 +11,15 @@ import models.Content;
 @RestController
 @RequestMapping("/api")
 public class ContentTestController {
-	private Logger logger = Logger.getLogger(ContentTestController.class);
 	
 	@RequestMapping(value = "/test/{id}", method = RequestMethod.GET)
 	public @ResponseBody Content getContent(
 	@PathVariable("id") 
 	long id) {
-		logger.info("inside /api/test/"+id);
 		
 		Content content = new Content();
 		content.setId(id);
 		content.setDescription("prova");
-		
-		logger.debug("result id="+content.getId()+" description="+content.getDescription() );
 		return content;
 		
 	}
