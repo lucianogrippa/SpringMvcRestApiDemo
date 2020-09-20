39bf4bdf3252d08d446d35900a3496dfd552cb71
Luciano Grippa <grippa.luciano@gmail.com>
2020-09-20 16:48:08 +0200

    added configuration server

---
+++ services/spring-mvc-rest-api-demo-config/src/main/java/com/grippaweb/server/SpringMvcRestApiDemoConfigApplication.java
@@ -0,0 +1,13 @@
+package com.grippaweb.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcRestApiDemoConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcRestApiDemoConfigApplication.class, args);
+	}
+
+}
---
+++ services/spring-mvc-rest-api-demo-config/src/test/java/com/grippaweb/server/SpringMvcRestApiDemoConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grippaweb.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcRestApiDemoConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
